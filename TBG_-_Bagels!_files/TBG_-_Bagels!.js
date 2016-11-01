7ff459eb75390b7bac1e4190b4284f0959d863da
macscuba <macscuba@mac.com>
2016-11-01 10:57:34 -0400

    revert

--- TBG_-_Bagels!_files/TBG_-_Bagels!.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20150916
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('TBG_-_Bagels!_files/TBG_-_Bagels!Moz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
