d2f33da210b1072fa0bf84c844621cbb499ff9a1
macscuba <macscuba@mac.com>
2017-05-07 15:06:07 -0400

    heck
    heck

---
+++ Travel/Heck_-_2002_files/Heck_-_2002.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20170507
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Heck_-_2002_files/Heck_-_2002Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
