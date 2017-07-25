a61b60a4f97f8ba58e5acf52188fb6371605a090
macscuba <macscuba@mac.com>
2017-07-25 15:17:19 -0400

    gc
    gc

---
+++ MacSCUBA/Grand_Cayman_2017_files/Grand_Cayman_2017.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20170725
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Grand_Cayman_2017_files/Grand_Cayman_2017Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id4');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
--- MacSCUBA/Roatan_2016_files/Roatan_2016.js
+++ MacSCUBA/Roatan_2016_files/Roatan_2016.js
@@ -1,9 +1,9 @@
-// Created by iWeb 3.0.4 local-build-20170222
+// Created by iWeb 3.0.4 local-build-20170725
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
-{loadMozillaCSS('Roatan_-_2016_files/Roatan_-_2016Moz.css')
+{loadMozillaCSS('Roatan_2016_files/Roatan_2016Moz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id4');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
