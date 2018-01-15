5095a43933c189b6934e6c78c2efc704d23d739d
macscuba <macscuba@mac.com>
2018-01-15 12:51:34 -0500

    mb
    bm

--- MacSawdust/Project_Photos_files/Project_Photos.js
+++ MacSawdust/Project_Photos_files/Project_Photos.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20171031
+// Created by iWeb 3.0.4 local-build-20180115
 
 function createMediaStream_id2()
 {return IWCreatePhotocast("http://www.mikeandgailk.com/MacSawdust/Project_Photos_files/rss.xml",true);}
