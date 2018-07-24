cf66cae256d86070212d6e17bbc29aa67ff14ea3
macscuba <macscuba@mac.com>
2018-07-24 13:47:55 -0400

    cex1

---
+++ MacSCUBA/CEX_I_2006_files/CEX_I_2006.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20180724
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('CEX_I_2006_files/CEX_I_2006Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
