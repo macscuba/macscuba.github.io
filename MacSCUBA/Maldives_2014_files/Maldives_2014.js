709bd542e45849360424319a9052f6707c8c5560
macscuba <macscuba@mac.com>
2019-10-27 10:32:24 -0400

    232

--- MacSCUBA/Maldives_2014_files/Maldives_2014.js
+++ MacSCUBA/Maldives_2014_files/Maldives_2014.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20191026
+// Created by iWeb 3.0.4 local-build-20191027
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,194),url:'Maldives_2014_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'Maldives_2014_files/stroke_1.png'},{rect:new IWRect(2,-2,296,4),url:'Maldives_2014_files/stroke_2.png'},{rect:new IWRect(298,-2,4,4),url:'Maldives_2014_files/stroke_3.png'},{rect:new IWRect(298,2,4,194),url:'Maldives_2014_files/stroke_4.png'},{rect:new IWRect(298,196,4,5),url:'Maldives_2014_files/stroke_5.png'},{rect:new IWRect(2,196,296,5),url:'Maldives_2014_files/stroke_6.png'},{rect:new IWRect(-2,196,4,5),url:'Maldives_2014_files/stroke_7.png'}],new IWSize(300,199))});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Maldives_2014_files/Maldives_2014Moz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');IMpreload('Maldives_2014_files','shapeimage_1','0');fixupIECSS3Opacity('id10');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id10');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
