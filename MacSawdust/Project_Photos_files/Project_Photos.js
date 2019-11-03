f53c951773a4f2d77c709ffb25c0da8dac60002f
macscuba <macscuba@mac.com>
2019-11-03 11:09:47 -0500

    3

--- MacSawdust/Project_Photos_files/Project_Photos.js
+++ MacSawdust/Project_Photos_files/Project_Photos.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20180929
+// Created by iWeb 3.0.4 local-build-20191103
 
 function createMediaStream_id2()
 {return IWCreatePhotocast("http://www.mikeandgailk.com/MacSawdust/Project_Photos_files/rss.xml",true);}
